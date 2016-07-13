b38cbff1d0ec9ce63be3672f18da5c672778eec6
Ben Cameron <ben.cameron@d3tech.net>
2016-07-13 09:44:56 -0500

    Updated gulp file to allow building app for publishing

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var autoprefixer = require('gulp-autoprefixer');
 var config = require('./gulp.config.json');
+var del = require('del');
 var gulp = require('gulp');
+var rev = require('gulp-rev');
+var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
+var usemin = require('gulp-usemin');
 
-/**
- * Compile SASS
- * @return {Stream}
- */
+gulp.task('build', function () {
+  runSequence('clean',
+    'compile-sass',
+    ['usemin', 'images', 'copy-app']);    
+});
+
+gulp.task('clean', function() {
+    return del([config.dist]);
+});
+
 gulp.task('compile-sass', function () {
     return gulp.src(config.sass)
         .pipe(sass().on('error', sass.logError))
@@ -17,11 +27,32 @@ gulp.task('compile-sass', function () {
         .pipe(gulp.dest(config.cssout));
 });
 
-gulp.task('build', ['compile-sass']);
+gulp.task('usemin', function() {
+    return gulp.src(config.html)
+        .pipe(usemin({
+            vendorcss: [ rev() ],
+            appcss: [ rev() ],
+            libjs: [ rev() ]
+        }))
+        .pipe(gulp.dest(config.dist));
+});
 
-/**
- * Watch files
- */
+gulp.task('images', function () {
+    var dest = config.dist + 'images';
+
+    return gulp
+        .src(config.images)
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('copy-app', function () {
+    var dest = config.dist + 'app';
+
+    return gulp
+        .src(config.app)
+        .pipe(gulp.dest(dest));
+});
+
 gulp.task('watch', function () {
     gulp.watch(config.sass, ['compile-sass']);
 });
